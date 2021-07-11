5f3faa6be7769ef09497b56731ac39e0e7910b7e
Bryan <bryantello2010@hotmail.com>
2021-07-10 22:34:56 -0600

    Agregada la clase de los estilos

--- frontend/src/components/ui/Navbar.js
+++ frontend/src/components/ui/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink, Link } from 'react-router-dom';
 
 
 export const Navbar = () => {
-    return <nav>
+    return <nav className="style_navbar">
         <Link className="nav-link" to='/'>Home</Link>
         <div className="link">
             <NavLink exact to='/'>
