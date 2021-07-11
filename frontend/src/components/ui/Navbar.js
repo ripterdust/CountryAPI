e7c1403884ff7cee8c00df011a94e1e3e681082f
Bryan <bryantello2010@hotmail.com>
2021-07-10 22:41:23 -0600

    Agregados estilos al navbar

--- frontend/src/components/ui/Navbar.js
+++ frontend/src/components/ui/Navbar.js
@@ -4,11 +4,11 @@ import { NavLink, Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return <nav className="style_navbar">
-        <NavLink exact to='/'>
+        <NavLink exact to='/' className="style_navbar_link" activeClassName="style_navbar_active">
             Countries
         </NavLink>
-        <Link className="nav-link" to='/'>Home</Link>
-        <NavLink to='/cities'>
+        <Link className="style_navbar_link_home" to='/'>Home</Link>
+        <NavLink to='/cities' className="style_navbar_link" activeClassName="style_navbar_active">
             Cities
         </NavLink>
     </nav>
