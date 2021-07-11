982d0cbf194608ba215b9fdaad390ef5e7686b13
Bryan <bryantello2010@hotmail.com>
2021-07-10 23:25:30 -0600

    Agregado el hero

--- frontend/src/components/mainPage/HomeScreen.js
+++ frontend/src/components/mainPage/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Hero } from './Hero'
 
 export const HomeScreen = () => {
     return (
         <div className="style_home">
-            Home page
+            <Hero />
         </div>
     )
 }
