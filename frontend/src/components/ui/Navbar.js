e025f698a0c5930b974f1123754913db824d03d9
Bryan <bryantello2010@hotmail.com>
2021-07-10 23:12:46 -0600

    terminados los estilos iniciales del navbar

--- frontend/src/components/ui/Navbar.js
+++ frontend/src/components/ui/Navbar.js
@@ -5,11 +5,11 @@ import { NavLink, Link } from 'react-router-dom';
 export const Navbar = () => {
     return <nav className="style_navbar">
         <NavLink exact to='/' className="style_navbar_link" activeClassName="style_navbar_active">
-            Country
+            Countries
         </NavLink>
         <Link className="style_navbar_link_home" to='/'>Home</Link>
         <NavLink to='/cities' className="style_navbar_link" activeClassName="style_navbar_active">
-            City
+            Cities
         </NavLink>
     </nav>
 }
